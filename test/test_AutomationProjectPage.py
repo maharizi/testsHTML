d59623f11eadb2da49f1dc8ac0d7d7783751fa0b
Maor Maharizi <maharizi96@gmail.com>
2023-02-17 10:37:18 +0200

    m

--- test/test_AutomationProjectPage.py
+++ test/test_AutomationProjectPage.py
@@ -2,7 +2,6 @@ from selenium.webdriver import Chrome
 from page.BasePage import BasePage
 from locator.LocatorAutomationProjectPage import Locator
 from dotenv import load_dotenv
-
 import os
 import sys
 import time
@@ -16,11 +15,7 @@ def automation_project_page():
     return BasePage(driver)
 
 
-@pytest.mark.test
+@pytest.mark.test_get_title
 def test_get_title(automation_project_page):
     assert automation_project_page.get_title() == "Automation Project"
 
-
-# @pytest.mark.test_first_test
-# def test_first_name(base_page):
-#     assert base_page.first_name_is_valid() == True
