82efa3cc6adc5edbd47256eeea8d453af64cefb5
refaeldimri <rafaeldimri@gmail.com>
2023-02-18 14:41:39 +0200

    added func to handle prompt alert

--- page/BasePage.py
+++ page/BasePage.py
@@ -154,19 +154,19 @@ class BasePage(object):
         else:
             return False
 
-    # def click_one_radio_button(self, option):
-    #     """
-    #     This function click
-    #     radio-button
-    #     :return null:
-    #     """
-    #     match option:
-    #         case 'Other':
-    #             self.driver.find_element(*A_Locator.other).click()
-    #         case 'Male':
-    #             self.driver.find_element(*A_Locator.male).click()
-    #         case 'Female':
-    #             self.driver.find_element(*A_Locator.female).click()
+    def click_one_radio_button(self, option):
+        """
+        This function click
+        radio-button
+        :return null:
+        """
+        match option:
+            case 'Other':
+                self.driver.find_element(*A_Locator.other).click()
+            case 'Male':
+                self.driver.find_element(*A_Locator.male).click()
+            case 'Female':
+                self.driver.find_element(*A_Locator.female).click()
 
     def one_radio_button_is_selected(self):
         """
@@ -182,29 +182,28 @@ class BasePage(object):
             return True
         return False
 
-
-    # def click_one_checkbox(self, options):
-    #     """
-    #     This function click
-    #     checkbox
-    #     :return null:
-    #     """
-    #     for option in options:
-        #     match option:
-        #         case 'Math':
-        #             self.driver.find_element(*A_Locator.math).click()
-        #         case 'Physics':
-        #             self.driver.find_element(*A_Locator.physics).click()
-        #         case 'POP':
-        #             self.driver.find_element(*A_Locator.pop).click()
-        #         case 'DUD':
-        #             self.driver.find_element(*A_Locator.dud).click()
-        #         case 'Biology':
-        #             self.driver.find_element(*A_Locator.biology).click()
-        #         case 'Chemistry':
-        #             self.driver.find_element(*A_Locator.chemistry).click()
-        #         case 'English':
-        #             self.driver.find_element(*A_Locator.english).click()
+    def click_one_checkbox(self, options):
+        """
+        This function click
+        checkbox
+        :return null:
+        """
+        for option in options:
+            match option:
+                case 'Math':
+                    self.driver.find_element(*A_Locator.math).click()
+                case 'Physics':
+                    self.driver.find_element(*A_Locator.physics).click()
+                case 'POP':
+                    self.driver.find_element(*A_Locator.pop).click()
+                case 'DUD':
+                    self.driver.find_element(*A_Locator.dud).click()
+                case 'Biology':
+                    self.driver.find_element(*A_Locator.biology).click()
+                case 'Chemistry':
+                    self.driver.find_element(*A_Locator.chemistry).click()
+                case 'English':
+                    self.driver.find_element(*A_Locator.english).click()
 
 
     def at_least_one_check_box_is_selected(self):
