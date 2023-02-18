39b7fd4ae3d683ff920dd6df720f64c6ea11e0e6
refaeldimri <rafaeldimri@gmail.com>
2023-02-18 19:33:12 +0200

    commit

--- main.py
+++ main.py
@@ -1,75 +1,19 @@
-import re
 import time
-import json
 
-from locator.LocatorAutomationProjectPage import Locator
-
-from selenium.webdriver import Chrome, ActionChains
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium import webdriver
-from utils.Util import *
-from page.BasePage import *
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver import Chrome
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.support.ui import Select
-from page.AutomationProjectPage import HomePage
+from selenium.common.exceptions import TimeoutException
+from locator.LocatorAutomationProjectPage import Locator as A_Locator
+from locator.LocatorNextPagePage import Locator as N_Locator
+import re
+
 from page.BasePage import BasePage
 
-
 if __name__ == '__main__':
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-=======
-    locator=Locator()
-    driver=Chrome()
-    a=BasePage(driver)
-
-    #a.insert_text(*Locator.first_name,text='shlomo')
-    #a.click_one_checkbox('Physics')
-    #a.insert_text(*Locator.paragraph_set_text, text='shlomo')
-
+    driver = Chrome()
+    bs = BasePage(driver)
     time.sleep(10)
-    print(a.check_paragraph_content())
-
-    input()
-    # name="shlomo"
-    # a.insert_text(*Locator.first_name,text='shlomo')
-    # time.sleep(5)
-    # print(a.get_text_from_text_box(*Locator.first_name))
-    # print(a.get_title())
->>>>>>> 68d9fadb21e0db333b1b4f8cd56183008a78cef6
-
-    #data=data_from_json()
-    #print(data[1]['Buttons'][1])
-    #testwriteToFile('refal','refal')
-
-   # print(data_from_json())
-
-    #url = "file:///C:/Users/rafae/PycharmProjects/pom_project/test/Automation%20Project.html"
-    #driver = webdriver.Chrome()
-    # home_page = HomePage(url, driver)
-    # home_page.driver.get(home_page.url)
-    # # driver = webdriver.Chrome()
-    #driver.get(url)
-    # # driver.find_element(By.CSS_SELECTOR, "a.btn").click()
-    # # a = driver.window_handles
-    # # driver.switch_to.window(a[0])
-    # # print(driver.title)
-    # print(home_page.driver.title)
-    #time.sleep(10)
-   #radio_button_female = driver.find_element(By.ID, "f").is_selected()
-    #print(radio_button_female)
-
-
-
-
+    print(bs.get_text_after_click_start_loading_button())
