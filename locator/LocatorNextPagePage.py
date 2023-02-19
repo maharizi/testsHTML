363228cf4d7372fedbc37474c392d10c081bf8dc
shlomo mhadker <shlomo22m@gmail.com>
2023-02-19 11:21:02 +0200

    test fix

--- locator/LocatorNextPagePage.py
+++ locator/LocatorNextPagePage.py
@@ -3,11 +3,7 @@ from selenium import webdriver
 
 
 class Locator(object):
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 060472c016a5faa15585ce5a1645edf7f21a5c3b
     # driver = webdriver.Chrome()
     # driver.get('file:///C:/bootcamp ness/python/html/nextpage.html')
     change_title = (By.XPATH, "//button")
@@ -17,10 +13,7 @@ class Locator(object):
     # #maor path-file:///C:/bootcamp ness/python/html/nextpage.html
     # driver.get('file:///C:/AutomationProject.html')
     # change_title = (By.XPATH, "//button")
-<<<<<<< HEAD
-=======
 
->>>>>>> 060472c016a5faa15585ce5a1645edf7f21a5c3b
 
 
 
--- main.py
+++ main.py
@@ -18,18 +18,17 @@ from page.AutomationProjectPage import HomePage
 from page.BasePage import BasePage
 
 
-<<<<<<< HEAD
 def f1(*args):
     print(args)
 
 
-if __name__ == '__main__':
-    a = HomePage(Chrome())
-    print(a.url)
+# if __name__ == '__main__':
+#     a = HomePage(Chrome())
+#     print(a.url)
 
 
 
-=======
+
 if __name__ == '__main__':
 
     path='utils/ddt.json'
@@ -37,19 +36,22 @@ if __name__ == '__main__':
     locator=Locator()
     driver=Chrome()
     a=BasePage(driver)
->>>>>>> 060472c016a5faa15585ce5a1645edf7f21a5c3b
+    b=HomePage(a.driver)
 
-    #a.insert_text(*Locator.first_name,text='shlomo')
+
+    a.insert_text(*Locator.email,text='shlomo22m@gmai.com')
+    print(b.email_is_valid())
+
     #a.click_one_checkbox('Physics')
     #a.insert_text(*Locator.paragraph_set_text, text='shlomo')
 
-<<<<<<< HEAD
+
 
     # name="shlomo"
     # a.insert_text(*Locator.first_name,text='shlomo')
     # time.sleep(5)
 
-=======
+
     # a.set_text_from_prompt_alert('hii')
     #a.check_paragraph_content()
     #time.sleep(5)
@@ -58,8 +60,8 @@ if __name__ == '__main__':
     #a.get_text_after_click_start_loading_button()
 
 
-    time.sleep(10)
-    print(a.check_paragraph_content())
+    #time.sleep(10)
+    #print(a.check_paragraph_content())
 
 
     #input()
@@ -67,12 +69,11 @@ if __name__ == '__main__':
     # a.insert_text(*Locator.first_name,text='shlomo')
     # time.sleep(5)
     # print(a.get_text_from_text_box(*Locator.first_name))
-    # print(a.get_title())
->>>>>>> 060472c016a5faa15585ce5a1645edf7f21a5c3b
 
 
-    data=data_from_json(path)
-    print(data[1]['Buttons'][0])
+
+    #data=data_from_json(path)
+    #print(data[1]['Buttons'][0])
     #testwriteToFile('refal','refal')
 
    # print(data_from_json())
